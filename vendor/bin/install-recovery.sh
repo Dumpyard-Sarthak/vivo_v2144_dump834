605042e0b42b76bd083c39e3ab4e91621c6b4283
sarthakroy2002 <sarthakroy2002@gmail.com>
2022-09-07 20:51:19 +0530

    Add vendor for sys_mssi_64_cn_armv82-user 13 TP1A.220624.003 eng.compil.20220810.234302 release-keys
    Signed-off-by: sarthakroy2002 <sarthakroy2002@gmail.com>

---
+++ vendor/bin/install-recovery.sh
@@ -0,0 +1,13 @@
+#!/vendor/bin/sh
+# added for checking whether if last normal-boot (after ota) finished or not      01/15/2019@chenyuqin
+/vendor/bin/dd if=/dev/zero of=/dev/block/bootdevice/by-name/reserved bs=1 count=8 seek=3672072 conv=notrunc && log -t recovery "succeeded to clear last-normal-boot-retry-count" || log -t recovery "failed to clear last-normal-boot-retry-count"
+/vendor/bin/dd if=/dev/zero of=/dev/block/bootdevice/by-name/reserved bs=1 count=8 seek=3672088 conv=notrunc && log -t recovery "succeeded to clear last-ota-to-boot-retry-count" || log -t recovery "failed to clear last-ota-to-boot-retry-count"
+if ! applypatch --check EMMC:/dev/block/by-name/recovery:201326592:d4648a71b7c548c12d8560c8f5100a43e59e1688; then
+  applypatch \
+          --flash /vendor/etc/recovery.img \
+          --target EMMC:/dev/block/by-name/recovery:201326592:d4648a71b7c548c12d8560c8f5100a43e59e1688 && \
+      log -t recovery "Installing new recovery image: succeeded" || \
+      log -t recovery "Installing new recovery image: failed"
+else
+  log -t recovery "Recovery image already installed"
+fi
